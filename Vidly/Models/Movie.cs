65a7aa6f95ee88d6bec61c881a1a5972ef675399
David Gilkeson <Gilky7@users.noreply.github.com>
2018-10-10 02:51:42 +0100

    Movie update

---
+++ Vidly/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+
+    //  /movies/random
+   
+
+    
+  
+}
